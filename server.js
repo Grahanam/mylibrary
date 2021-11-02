233e0a6c2b07817054146774807e87c25be83a79
Grahanam <lunasuthar5221@gmail.com>
2021-11-02 16:32:33 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 if(process.env.NODE_ENV !=='production'){
     require('dotenv').config()
 }
-
+require('dotenv').config()
 
 
 const express=require('express')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
+const bodyParser=require('body-parser')
 
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+//app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+app.use(express.urlencoded({limit:'10mb',extended:false}));
 
 
 const mongoose=require('mongoose')
@@ -25,5 +28,8 @@ db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT||3000)
+
+//<%  (typeof errorMessage != "undefined" ? errorMessage : "") %>
